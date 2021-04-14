bda120533b5a6c02db7c4057faff6f4347af6971
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2021-04-14 16:26:09 +0200

    adding products

--- js/product.js
+++ js/product.js
@@ -1,5 +1,6 @@
 const url = "http://localhost/wordpress/wp-json/wc/store/products";
 const productContainer = document.querySelector(".products");
+const infoContainer = document.querySelector(".infoContainer");
 
 async function getProducts() {
   try {
@@ -15,9 +16,13 @@ getProducts();
 
 function createHTML(products) {
   products.forEach(function (product) {
-    productContainer.innerHTML += `<div class="product">
-        <h2>${product.name}</h2>
-        <img src="${product.images[0].src}" alt="${product.name}"
-        </div>`;
+    productContainer.innerHTML += `<a href="../Shop/product-page.html">
+        <div class="product">
+        <h2 class="productTitle">${product.name}</h2>
+        <img src="${product.images[0].src}" alt="${product.name}"/>
+        <p class="productPrice">£${product.prices.price}</p>
+        ${product.description}
+        </div>
+        </a>`;
   });
 }
